9727d84d45f6c5aab3c74c2a56d873b2c5d9a817
piedra-de-flor <xxspdlqjxx@naver.com>
2025-10-25 17:29:48 +0900

    Initialize Spring Boot Kotlin project

---
+++ src/main/kotlin/com/example/home_recipe/HomeRecipeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.home_recipe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomeRecipeApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomeRecipeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/home_recipe/HomeRecipeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.home_recipe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HomeRecipeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
