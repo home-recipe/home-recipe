2644c325c9c6b33ed8964649a63623362874e292
piedra-de-flor <xxspdlqjxx@naver.com>
2025-10-29 16:50:21 +0900

    feat : 냉장고 Service, Repository, Controller 정의
    - Service, Repository, Controller 정의
    
    #8

---
+++ src/main/kotlin/com/example/home_recipe/controller/refrigerator/RefrigeratorController.java
@@ -0,0 +1,7 @@
+package com.example.home_recipe.controller.refrigerator;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RefrigeratorController {
+}
