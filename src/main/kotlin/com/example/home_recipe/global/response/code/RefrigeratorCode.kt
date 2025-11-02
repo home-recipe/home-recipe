c1e616b5366d1277cbbc8c424f5dd4d45faed4e0
piedra-de-flor <xxspdlqjxx@naver.com>
2025-11-02 23:27:50 +0900

    feat : 냉장고 재료 추가 및 사용 Service 로직 구현
    - 재료 사용 및 추가 메서드 구현
    - 유저 or 냉장고 or 재료 탐색 실패시 예외
    - Set에 중복 재료 있을 시 예외
    
    #9 #10

--- src/main/kotlin/com/example/home_recipe/global/response/code/RefrigeratorCode.kt
+++ src/main/kotlin/com/example/home_recipe/global/response/code/RefrigeratorCode.kt
@@ -5,5 +5,6 @@ enum class RefrigeratorCode(override val code: String, override val message: Str
     REFRIGERATOR_ERROR_001("REFRIGERATOR_ERROR_001", "요청 파라미터 오류"),
     REFRIGERATOR_ERROR_002("REFRIGERATOR_ERROR_002", "로그인하지 않음"),
     REFRIGERATOR_ERROR_003("REFRIGERATOR_ERROR_003", "다른 사용자의 냉장고 접근"),
-    REFRIGERATOR_ERROR_004("REFRIGERATOR_ERROR_004", "냉장고 없음")
+    REFRIGERATOR_ERROR_004("REFRIGERATOR_ERROR_004", "냉장고 없음"),
+    REFRIGERATOR_ERROR_005("REFRIGERATOR_ERROR_005", "이미 냉장고에 있는 재료입니다")
 }
