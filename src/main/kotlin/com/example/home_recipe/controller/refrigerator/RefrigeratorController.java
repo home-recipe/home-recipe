0cd4f4470a4e7dbb25152bc6b10b00e05a587af3
piedra-de-flor <xxspdlqjxx@naver.com>
2025-11-02 23:57:02 +0900

    feat : 냉장고 생성, 재료 추가 및 사용 Controller API 구현
    - 재료 사용 및 추가 API 구현
    - 냉장고 관련 RESPONSE_CODE 추가
    
    #8 #9 #10

--- src/main/kotlin/com/example/home_recipe/controller/refrigerator/RefrigeratorController.java
+++
@@ -1,7 +0,0 @@
-package com.example.home_recipe.controller.refrigerator;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class RefrigeratorController {
-}
