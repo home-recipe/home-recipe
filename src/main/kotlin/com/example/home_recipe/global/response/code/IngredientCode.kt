8ddacef2c22066938a6251da938813d201c67c82
piedra-de-flor <xxspdlqjxx@naver.com>
2025-11-03 19:16:43 +0900

    feat : 재료 DTO 정의
    - 생성 request DTO 정의
    - 수정 request DTO 정의
    - 재료 response DTO 정의
    
    #11 #12

--- src/main/kotlin/com/example/home_recipe/global/response/code/IngredientCode.kt
+++ src/main/kotlin/com/example/home_recipe/global/response/code/IngredientCode.kt
@@ -6,5 +6,7 @@ enum class IngredientCode(override val code: String, override val message: Strin
     INGREDIENT_ERROR_006("INGREDIENT_ERROR_006", "비로그인 요청"),
     INGREDIENT_ERROR_007("INGREDIENT_ERROR_007", "다른 사용자 냉장고 접근"),
     INGREDIENT_ERROR_008("INGREDIENT_ERROR_008", "잘못된 재료 ID"),
-    INGREDIENT_ERROR_011("INGREDIENT_ERROR_011", "해당 재료 없음")
+    INGREDIENT_ERROR_011("INGREDIENT_ERROR_011", "해당 재료 없음"),
+    INGREDIENT_ERROR_012("INGREDIENT_ERROR_012", "재료 이름 제한 조건 불만족"),
+    INGREDIENT_ERROR_013("INGREDIENT_ERROR_013", "재료 카테고리 누락")
 }
