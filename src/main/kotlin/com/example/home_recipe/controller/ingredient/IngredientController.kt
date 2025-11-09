b85f14591296ed4524b1c3e460bae1766187d6b0
piedra-de-flor <xxspdlqjxx@naver.com>
2025-11-09 18:09:47 +0900

    feat : 재료 리스트 생성 API 구현 및 api 권한 admin으로 변경
    - Controller에 "/admin" 추가
    - Controller에 API 추가
    
    #12

--- src/main/kotlin/com/example/home_recipe/controller/ingredient/IngredientController.kt
+++ src/main/kotlin/com/example/home_recipe/controller/ingredient/IngredientController.kt
@@ -1,5 +1,6 @@
 package com.example.home_recipe.controller.ingredient
 
+import com.example.home_recipe.controller.dto.ingredient.CreateIngredientBatchRequest
 import com.example.home_recipe.controller.dto.ingredient.CreateIngredientRequest
 import com.example.home_recipe.controller.dto.ingredient.IngredientResponse
 import com.example.home_recipe.controller.dto.ingredient.UpdateIngredientRequest
@@ -7,12 +8,13 @@ import com.example.home_recipe.global.response.ApiResponse
 import com.example.home_recipe.global.response.code.IngredientCode
 import com.example.home_recipe.service.ingredient.IngredientService
 import jakarta.validation.Valid
+import jakarta.validation.constraints.Size
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/ingredients")
+@RequestMapping("/admin/ingredients")
 class IngredientController(
     private val ingredientService: IngredientService
 ) {
@@ -20,6 +22,12 @@ class IngredientController(
     @PostMapping
     fun create(@Valid @RequestBody request: CreateIngredientRequest): ResponseEntity<ApiResponse<IngredientResponse>> {
         val result = ingredientService.create(request)
+        return ApiResponse.success(result, IngredientCode.CREATE_SUCCESS, HttpStatus.CREATED)
+    }
+
+    @PostMapping("/batch") fun createAll(
+        @Valid @RequestBody request: CreateIngredientBatchRequest): ResponseEntity<ApiResponse<List<IngredientResponse>>> {
+        val result = ingredientService.createAll(request.items)
         return ApiResponse.success(result, IngredientCode.CREATE_SUCCESS, HttpStatus.CREATED)
     }
 
