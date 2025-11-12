e793d8fa4637a6ac6a053ca92e7ae47ddc8a61e2
bingsuzoa <zxc_777@naver.com>
2025-11-12 23:43:41 +0900

    refactor: ApiResponse 구조 개선 및 data null 허용 처리 - ApiResponse와 ResponseDetail 제네릭 구조 수정 - data 필드에 null 허용 (T? 적용) - success() / error() 공통 로직 통일 및 반환 타입 단순화 - data가 없는 응답도 일관된 포맷으로 처리 가능하도록 개선
    #2

--- src/main/kotlin/com/example/home_recipe/global/response/ApiResponse.kt
+++ src/main/kotlin/com/example/home_recipe/global/response/ApiResponse.kt
@@ -4,14 +4,17 @@ import com.example.home_recipe.global.response.code.BaseCode
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
-class ApiResponse<T> (
+class ApiResponse<T>(
     val code: Int,
     val message: String,
     val response: ResponseDetail<T?>
 ) {
     companion object {
-        fun <T> success(data: T, responseCode: BaseCode, status: HttpStatus = HttpStatus.OK)
-        : ResponseEntity<ApiResponse<T>> {
+        fun <T> success(
+            data: T? = null,
+            responseCode: BaseCode,
+            status: HttpStatus = HttpStatus.OK
+        ): ResponseEntity<ApiResponse<T>> {
             val body = ApiResponse(
                 code = status.value(),
                 message = responseCode.message,
@@ -23,15 +26,17 @@ class ApiResponse<T> (
             return ResponseEntity.status(status).body(body)
         }
 
-        fun error(responseCode: BaseCode, status: HttpStatus)
-                : ResponseEntity<ApiResponse<Unit>> {
-
+        fun <T> error(
+            data: T? = null,
+            responseCode: BaseCode,
+            status: HttpStatus
+        ): ResponseEntity<ApiResponse<T>> {
             val body = ApiResponse(
                 code = status.value(),
                 message = responseCode.message,
                 response = ResponseDetail(
                     code = responseCode.code,
-                    data = null as Unit?
+                    data = data
                 )
             )
             return ResponseEntity.status(status).body(body)
