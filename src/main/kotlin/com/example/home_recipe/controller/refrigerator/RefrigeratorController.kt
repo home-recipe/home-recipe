e42d1bf28d2b8c4af9800cf48b3d1d804b9f4bc1
piedra-de-flor <xxspdlqjxx@naver.com>
2025-11-24 21:31:52 +0900

    refactor : @UserEmail 어노테이션 -> @AuthenticationPrincipal로 변경
    - RefrigeratorController 에서 사용되면 user의 email 검출인 @UserEmail 어노테이션이 @AuthenticationPrincipal로 변경됨

--- src/main/kotlin/com/example/home_recipe/controller/refrigerator/RefrigeratorController.kt
+++ src/main/kotlin/com/example/home_recipe/controller/refrigerator/RefrigeratorController.kt
@@ -2,10 +2,10 @@ package com.example.home_recipe.controller.refrigerator
 
 import com.example.home_recipe.global.response.ApiResponse
 import com.example.home_recipe.global.response.code.RefrigeratorCode
-import com.example.home_recipe.global.sercurity.annotation.UserEmail
 import com.example.home_recipe.service.refrigerator.RefrigeratorService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -14,19 +14,19 @@ class RefrigeratorController(
     private val refrigeratorService: RefrigeratorService
 ) {
     @PostMapping
-    fun create(@UserEmail email: String): ResponseEntity<ApiResponse<Boolean>> {
+    fun create(@AuthenticationPrincipal email: String): ResponseEntity<ApiResponse<Boolean>> {
         refrigeratorService.createForUser(email)
         return ApiResponse.success(true, RefrigeratorCode.CREATE_SUCCESS, HttpStatus.CREATED)
     }
 
     @PutMapping("/ingredient/{ingredientId}")
-    fun add(@UserEmail email: String, @PathVariable ingredientId: Long): ResponseEntity<ApiResponse<Boolean>> {
+    fun add(@AuthenticationPrincipal email: String, @PathVariable ingredientId: Long): ResponseEntity<ApiResponse<Boolean>> {
         refrigeratorService.addIngredient(email, ingredientId)
         return ApiResponse.success(true, RefrigeratorCode.ADD_INGREDIENT_SUCCESS, HttpStatus.OK)
     }
 
     @DeleteMapping("/ingredient/{ingredientId}")
-    fun use(@UserEmail email: String, @PathVariable ingredientId: Long): ResponseEntity<ApiResponse<Boolean>> {
+    fun use(@AuthenticationPrincipal email: String, @PathVariable ingredientId: Long): ResponseEntity<ApiResponse<Boolean>> {
         refrigeratorService.useIngredient(email, ingredientId)
         return ApiResponse.success(true, RefrigeratorCode.USE_INGREDIENT_SUCCESS, HttpStatus.OK)
     }
