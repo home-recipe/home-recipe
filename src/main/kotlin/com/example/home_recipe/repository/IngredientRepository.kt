c6567e9f57c21c6ea153814660f1ac7ceaf1c541
piedra-de-flor <xxspdlqjxx@naver.com>
2025-12-06 22:21:11 +0900

    refactor : 기본 재료 냉장고 생성시 추가 로직 변경
    - 이벤트 기반 -> 단순 Service 로직 호출로 변경
    - 이미 냉장고가 재료에 대한 참조를 하고 있기 때문에 굳이 이벤트 기반을 사용할 이유가 없음
    
    #026

--- src/main/kotlin/com/example/home_recipe/repository/IngredientRepository.kt
+++ src/main/kotlin/com/example/home_recipe/repository/IngredientRepository.kt
@@ -1,9 +1,11 @@
 package com.example.home_recipe.repository
 
 import com.example.home_recipe.domain.ingredient.Ingredient
+import com.example.home_recipe.domain.ingredient.IngredientCategory
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface IngredientRepository : JpaRepository<Ingredient, Long> {
+    fun findByCategoryAndName(category: IngredientCategory, name: String): Ingredient?
 }
