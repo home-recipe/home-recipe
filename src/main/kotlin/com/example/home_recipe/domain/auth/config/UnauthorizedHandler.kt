7f80e69fe18959eedfa4b30dbfaad79d02803d0b
bingsuzoa <zxc_777@naver.com>
2025-12-09 05:46:55 +0900

    chore : 코드 정렬

--- src/main/kotlin/com/example/home_recipe/domain/auth/config/UnauthorizedHandler.kt
+++ src/main/kotlin/com/example/home_recipe/domain/auth/config/UnauthorizedHandler.kt
@@ -1,10 +1,8 @@
 package com.example.home_recipe.domain.auth.config
 
-import com.example.home_recipe.global.exception.BusinessException
 import com.example.home_recipe.global.response.code.AuthCode
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.http.HttpStatus
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
@@ -15,6 +13,7 @@ class UnauthorizedHandler : AuthenticationEntryPoint {
     companion object {
         const val CONTENT_TYPE = "application/json;charset=UTF-8"
     }
+
     override fun commence(
         request: HttpServletRequest,
         response: HttpServletResponse,
