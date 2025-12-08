79308350c7f5eebf1458d615709450de9711d530
bingsuzoa <zxc_777@naver.com>
2025-12-09 06:39:31 +0900

    test: 테스트 환경 H2로 통일 및 내장 DB 자동 교체 비활성화
    - Spring Boot가 기본 embedded H2로 DataSource를 교체하면서 발생하던 스키마 생성 오류 해결
    - @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) 적용
    - application-test.yml 기반의 H2 설정이 정상 동작하도록 테스트 환경 통일
    - ddl-auto=create 문제 및 join table 생성 오류 해결

--- src/test/kotlin/com/example/home_recipe/HomeRecipeApplicationTests.kt
+++ src/test/kotlin/com/example/home_recipe/HomeRecipeApplicationTests.kt
@@ -7,6 +7,7 @@ import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class HomeRecipeApplicationTests {
 
 	@Test
