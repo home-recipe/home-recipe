83554fd8c36431c0c0362f56b884658a3a02345a
bingsuzoa <zxc_777@naver.com>
2025-12-09 22:18:28 +0900

    refactor: 재료 추천 기능을 문자열 입력 방식 → User 냉장고 기반 조회 방식으로 변경 - 조회된 재료 리스트를 기반으로 GPT Prompt 생성 로직 수정
    #17

--- src/main/kotlin/com/example/home_recipe/service/recommendation/RecommendationService.kt
+++ src/main/kotlin/com/example/home_recipe/service/recommendation/RecommendationService.kt
@@ -2,6 +2,7 @@ package com.example.home_recipe.service.recommendation
 
 import com.example.home_recipe.controller.recommendation.dto.RecommendationsResponse
 import com.example.home_recipe.service.recipe.RecipePrompt
+import com.example.home_recipe.service.user.UserService
 import com.openai.client.okhttp.OpenAIOkHttpClientAsync
 import com.openai.models.ChatModel
 import com.openai.models.chat.completions.ChatCompletionCreateParams
@@ -10,17 +11,18 @@ import org.springframework.stereotype.Service
 
 @Service
 class RecommendationService(
-    @Value("\${openai.api-key}") val apiKey : String
+    @Value("\${openai.api-key}") val apiKey : String,
+    val userService: UserService
 ) {
 
     val client = OpenAIOkHttpClientAsync.builder()
         .apiKey(apiKey)
         .build()
 
-    fun chat(input: String) : RecommendationsResponse {
+    fun chat(email: String) : RecommendationsResponse {
         val params = ChatCompletionCreateParams.builder()
             .addSystemMessage(RecommendationPrompt.SYSTEM_PROMPT)
-            .addUserMessage(RecipePrompt.userPrompt(input))
+            .addUserMessage(RecommendationPrompt.userPrompt(userService.getAllIngredientsOfUser(email)))
             .model(ChatModel.GPT_5_MINI)
             .responseFormat(RecommendationsResponse::class.java)
             .build()
