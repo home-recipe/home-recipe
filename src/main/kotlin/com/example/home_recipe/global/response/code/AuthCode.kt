8ca1336c51387c053e5a15798e87a3db051b0fb3
bingsuzoa <zxc_777@naver.com>
2025-12-26 05:45:47 +0900

    fix : 오류 메시지 토큰 오류에 맞게 세부적으로 변경 #33

--- src/main/kotlin/com/example/home_recipe/global/response/code/AuthCode.kt
+++ src/main/kotlin/com/example/home_recipe/global/response/code/AuthCode.kt
@@ -6,9 +6,13 @@ enum class AuthCode (
 ) : BaseCode {
 
     AUTH_INVALID_TOKEN("AUTH_001", "토큰이 유효하지 않습니다."),
-    AUTH_REQUIRED_LOGIN("AUTH_002", "로그인이 필요합니다."),
-    AUTH_EXPIRED_TOKEN("AUTH_003", "토큰의 유효기간이 만료되었습니다."),
-    AUTH_FORBIDDEN("AUTH_004", "제한된 접근입니다."),
+    AUTH_EXPIRED_TOKEN("AUTH_002", "토큰이 만료되었습니다."),
+    AUTH_NOT_EXIST_TOKEN("AUTH_003", "인증정보가 없습니다."),
+    AUTH_INVALID_CREDENTIALS("AUTH_004", "이메일 또는 비밀번호가 올바르지 않습니다"),
+    AUTH_FORBIDDEN("AUTH_005", "제한된 접근입니다."),
+
+    AUTH_REFRESH_EXPIRED_TOKEN("AUTH_006", "Refresh Token이 만료되었습니다."),
+    AUTH_REFRESH_INVALID_TOKEN("AUTH_007", "Refresh TOken이 유효하지 않습니다."),
 
     AUTH_LOGIN_SUCCESS("AUTH_LOGIN_SUCCESS", "로그인 성공"),
     AUTH_LOGOUT_SUCCESS("AUTH_LOGOUT_SUCCESS", "로그아웃 성공"),
