c46bbd29135a0342321636ea3eaefce88db7ade2
bingsuzoa <zxc_777@naver.com>
2025-12-26 05:48:27 +0900

    fix: @AuthenticationPrincipal 대신 Authentication 사용
    - principal 타입 불일치로 email 주입되지 않는 문제 수정
    
    #33

--- src/main/kotlin/com/example/home_recipe/controller/refrigerator/RefrigeratorController.kt
+++ src/main/kotlin/com/example/home_recipe/controller/refrigerator/RefrigeratorController.kt
@@ -6,6 +6,7 @@ import com.example.home_recipe.global.response.code.RefrigeratorCode
 import com.example.home_recipe.service.refrigerator.RefrigeratorService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
@@ -15,20 +16,20 @@ class RefrigeratorController(
     private val refrigeratorService: RefrigeratorService
 ) {
     @PostMapping
-    fun create(@AuthenticationPrincipal principal : EmailPrincipal): ResponseEntity<ApiResponse<Boolean>> {
-        refrigeratorService.createForUser(principal.email)
+    fun create(authentication: Authentication): ResponseEntity<ApiResponse<Boolean>> {
+        refrigeratorService.createForUser(authentication.name)
         return ApiResponse.success(true, RefrigeratorCode.CREATE_SUCCESS, HttpStatus.CREATED)
     }
 
     @PutMapping("/ingredient/{ingredientId}")
-    fun add(@AuthenticationPrincipal principal: EmailPrincipal, @PathVariable ingredientId: Long): ResponseEntity<ApiResponse<Boolean>> {
-        refrigeratorService.addIngredient(principal.email, ingredientId)
+    fun add(authentication: Authentication, @PathVariable ingredientId: Long): ResponseEntity<ApiResponse<Boolean>> {
+        refrigeratorService.addIngredient(authentication.name, ingredientId)
         return ApiResponse.success(true, RefrigeratorCode.ADD_INGREDIENT_SUCCESS, HttpStatus.OK)
     }
 
     @DeleteMapping("/ingredient/{ingredientId}")
-    fun use(@AuthenticationPrincipal principal: EmailPrincipal, @PathVariable ingredientId: Long): ResponseEntity<ApiResponse<Boolean>> {
-        refrigeratorService.useIngredient(principal.email, ingredientId)
+    fun use(authentication: Authentication, @PathVariable ingredientId: Long): ResponseEntity<ApiResponse<Boolean>> {
+        refrigeratorService.useIngredient(authentication.name, ingredientId)
         return ApiResponse.success(true, RefrigeratorCode.USE_INGREDIENT_SUCCESS, HttpStatus.OK)
     }
 }
