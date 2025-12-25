604710ef4842e06b063c00d9796c840e1302dcab
bingsuzoa <zxc_777@naver.com>
2025-12-26 06:26:31 +0900

    fix : RestApi /api 시작하는 주소로 명칭 통일 #33

--- src/main/kotlin/com/example/home_recipe/controller/auth/dto/request/RefreshTokenRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.example.home_recipe.controller.auth.dto.request
-
-import jakarta.validation.constraints.NotNull
-
-data class RefreshTokenRequest(
-    @NotNull
-    val refreshToken: String
-)
