b590088a05a06b09c8641327eb1cf0b9313558b4
bingsuzoa <zxc_777@naver.com>
2025-12-27 16:04:44 +0900

    refactor: Recipe·Recommendation 컨트롤러 응답 구조 정리
    - Recipe, Recommendation 컨트롤러 응답 포맷 통일
    - getIngredients 메서드를 이름만 조회하도록 명확한 네이밍으로 변경
    - Controller 테스트 추가
    
    #33

--- src/main/kotlin/com/example/home_recipe/controller/recommendation/RecommendationController.kt
+++ src/main/kotlin/com/example/home_recipe/controller/recommendation/RecommendationController.kt
@@ -2,7 +2,11 @@ package com.example.home_recipe.controller.recommendation
 
 import com.example.home_recipe.controller.recommendation.dto.RecommendationsResponse
 import com.example.home_recipe.controller.user.dto.response.EmailPrincipal
+import com.example.home_recipe.global.response.ApiResponse
+import com.example.home_recipe.global.response.code.RecommendationCode
 import com.example.home_recipe.service.recommendation.RecommendationService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.PostMapping
@@ -16,7 +20,9 @@ class RecommendationController(
 ) {
 
     @PostMapping("/recommendation")
-    fun getRecommendation(authentication: Authentication): RecommendationsResponse {
-        return recommendationService.chat(authentication.name)
+    fun getRecommendation(authentication: Authentication): ResponseEntity<ApiResponse<RecommendationsResponse>> {
+        return ApiResponse.success(recommendationService.chat(authentication.name),
+            RecommendationCode.RECOMMENDATION_SUCCESS, HttpStatus.CREATED
+        )
     }
 }
