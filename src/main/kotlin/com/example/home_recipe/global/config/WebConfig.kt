2062cd0b1c48722754b6fc81807fc22a1b22b56d
mikyeong Kwon <zxc_777@naver.com>
2026-02-27 08:16:43 +0900

    fix : local, prod 환경에서의 설정 분리

--- src/main/kotlin/com/example/home_recipe/global/config/WebConfig.kt
+++ src/main/kotlin/com/example/home_recipe/global/config/WebConfig.kt
@@ -1,6 +1,8 @@
 package com.example.home_recipe.global.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -8,12 +10,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig : WebMvcConfigurer {
 
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOrigins("http://recook.kr:3000", "http://recook.kr", "https://recook.kr")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true)
+    @Bean
+    @Profile("local")
+    fun localCorsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("*")
+                    .allowCredentials(true)
+            }
+        }
+    }
+
+    @Bean
+    @Profile("prod")
+    fun prodCorsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://recook.kr:3000", "http://recook.kr", "https://recook.kr")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                    .allowCredentials(true)
+            }
+        }
     }
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
