6bdfa0cd702f949fe80459ad0b843966baababb7
mikyeong Kwon <zxc_777@naver.com>
2026-02-27 16:24:43 +0900

    fix : 웹 일때, loginResponse에 refreshToken 전달하지 않도록 수정

--- src/main/kotlin/com/example/home_recipe/controller/auth/dto/response/LoginResponse.kt
+++ src/main/kotlin/com/example/home_recipe/controller/auth/dto/response/LoginResponse.kt
@@ -1,9 +1,11 @@
 package com.example.home_recipe.controller.auth.dto.response
 
 import com.example.home_recipe.domain.user.Role
+import com.fasterxml.jackson.annotation.JsonInclude
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class LoginResponse(
     val accessToken: String,
-    val refreshToken: String,
+    val refreshToken: String? = null,
     val role: Role
 )
